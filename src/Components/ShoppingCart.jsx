e3e011075f4984b8c6da051e118175c3006ad6d7
xdlp <sdlopezp@correo.udistrital.edu.co>
2025-05-08 20:07:17 -0500

    primeras adiciones de reducers. toca estudiar más

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -8,7 +8,7 @@ const ShoppingCart = () => {
     <div className="shopping-cart">
       <h2 className="shopping-cart-title">Shopping Cart</h2>
       <ul className="cart-items">
-       
+        
       </ul>
       <button className="clear-cart-btn">Clear Cart</button>
     </div>
